8b50fc500b10f9bb55537bd216b75ab3a8069c04
SLT6723 <stephenthomas@stephens-mac-mini.local>
2017-08-11 20:56:47 -0400

    Cleaned up project and created EventListViewController

---
+++ TheTea/TheTea/Event List/EventListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  EventListViewController.swift
+//  TheTea
+//
+//  Created by Stephen Thomas on 8/11/17.
+//  Copyright © 2017 The Tea LLC. All rights reserved.
+//
+
+import UIKit
+
+class EventListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "The Tea"
+        view.backgroundColor = .white
+    }
+}
